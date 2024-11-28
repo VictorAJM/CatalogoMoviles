8a1590652a1290dd37fac1f3b22d43fa9728020f
VictorAJM <Victorajm@outlook.com>
2024-11-28 16:32:43 -0600

    Carrito clase con algunos ejemplos

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, SafeAreaView, FlatList } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -6,96 +6,57 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import Category from '@/components/Category';
+import Carrito from '@/components/Carrito';
 
 export default function TabTwoScreen() {
+
+  const carritos = [
+    {id: '1', name: 'PUNK ROD', year: 2024, Category:'Rod Squad', categoryID: 1,  hwID: 'HRY48'},
+    {id: '2', name: 'HONDA CB750 CAFÉ', year: 2024, Category:'Rod Squad', categoryID: 2, hwID: 'HTC61'},
+    {id: '3', name: 'LIMITED GRIP', year: 2024, Category:'Rod Squad', categoryID: 3, hwID: 'HTC62'},
+    {id: '4', name: "CUSTOM '53 CHEVY", year: 2024, Category:'Rod Squad', categoryID: 4, hwID: 'HTF18'},
+    {id: '5', name: 'HIROHATA MERC', year: 2024, Category:'Rod Squad', categoryID: 5, hwID: 'HTD66'},
+    {id: '6', name: 'SHARK BITE', year: 2024, Category:'HW Mega Bite', categoryID: 1, hwID: 'HTC01'},
+    {id: '7', name: 'CROC ROD', year: 2024, Category:'HW Mega Bite', categoryID: 2, hwID: 'HTC02'},
+  ];
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={carritos}
+          renderItem={({ item }) => (
+            <Carrito
+              name={item.name}
+              year={item.year}
+              category={item.Category}
+              categoryID={item.categoryID}
+              hwID={item.hwID}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
   headerImage: {
     color: '#808080',
     bottom: -90,
@@ -105,5 +66,12 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  row: {
+    justifyContent: 'space-between', // Espaciado entre columnas
   },
 });
---
+++ components/Carrito.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+interface CarritoProps {
+  name: string;
+  year: number;
+  category: string;
+  categoryID: number;
+  hwID: string;
+}
+
+
+const Carrito: React.FC<CarritoProps> = ({
+  name, year, category, categoryID, hwID
+}) => {
+  return <View style={styles.card}>
+    <Text style={styles.categoryTitle}>{`${category} - ${year}  (${categoryID}/total)`}</Text>
+    <Text style={styles.title}>{name}</Text>
+    <Text style={styles.hwText}>{hwID}</Text>
+  </View>;
+}
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    margin: 10,
+    padding: 15,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  categoryTitle: {
+    fontSize: 10,
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  hwText: {
+    fontSize: 12,
+    marginBottom: 5,
+  },
+});
+
+export default Carrito;
