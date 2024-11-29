5cb4db74c8f5bea55ddbe5083069ad25389c7e63
VictorAJM <Victorajm@outlook.com>
2024-11-28 18:32:55 -0600

    adding category to database

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,17 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useColorScheme } from '@/hooks/useColorScheme';
-
+import HomeScreen from './(tabs)';
+import addCategory from './addCategory';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
+const Stack = createNativeStackNavigator();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
@@ -28,13 +29,9 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-        <Stack.Screen name="addCategory"/>
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="addCategory" component={addCategory} />
+      </Stack.Navigator>
   );
 }
--- app/addCategory.tsx
+++ app/addCategory.tsx
@@ -1,18 +1,62 @@
 import React, { useEffect, useState } from 'react';
+import * as SQLite from 'expo-sqlite';
 import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
-
-export default function addCategory({navigation }: any) {
+import Toast from 'react-native-toast-message';
+export default function addCategory({ navigation }) {
 
   const [name, setName] = useState('');
   const [year, setYear] = useState('');
   const [quantity, setQuantity] = useState('');
 
-  const handleSubmit = () => {
-    console.log('Name:', name);
-    console.log('Year:', year);
-    console.log('cantidad', quantity);
-    
-    navigation.goBack();
+  const showToast = () => {
+    Toast.show({
+      type: 'success', // Puedes elegir 'success', 'error', o 'info'
+      position: 'bottom', // También puede ser 'top' o 'center'
+      text1: '¡Éxito!',
+      text2: 'El mensaje se ha enviado correctamente.',
+      visibilityTime: 3000, // Tiempo en milisegundos que el Toast será visible
+      autoHide: true, // Si el Toast se oculta automáticamente
+    });
+  };
+
+  const handleSubmit = async () => {
+    const db = await SQLite.openDatabaseAsync('databaseName');
+    await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS testCategory (
+        id INTEGER PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL, 
+        year INTEGER,
+        quantity INTEGER);
+      `);
+      await db.runAsync('INSERT INTO testCategory (name,year,quantity) VALUES (?, ?, ?)', "a",20, 1);
+      const allRows = await db.getAllAsync('SELECT * FROM testCategory');
+      for (const row of allRows) {
+        
+        if (row.name == name) {
+          console.log(row.name);
+          Toast.show({
+            type: 'error', // Tipo de Toast, 'error' para error
+            position: 'top', // Posición del Toast en la pantalla
+            text1: 'Categoría ya existente',
+            text2: 'La categoría con este nombre ya está registrada.',
+            visibilityTime: 3000,
+            autoHide: true,
+          });
+          return;
+        }
+      }
+      
+      const result = await db.runAsync('INSERT INTO testCategory (name,year,quantity) VALUES (?, ?, ?)', name,year, quantity);
+      //mostrar toast de que funciono correctamente
+      Toast.show({
+        type: 'success', // Tipo de Toast, 'success' para éxito
+        position: 'top',
+        text1: 'Categoría guardada',
+        text2: 'La categoría se ha guardado correctamente.',
+        visibilityTime: 3000,
+        autoHide: true,
+      });
+      return;
   };
 
   return (
@@ -37,6 +81,7 @@ export default function addCategory({navigation }: any) {
       <TextInput
         style={styles.input}
         placeholder="cantidad de objetos en la categoría"
+        keyboardType="numeric"
         value={quantity}
         onChangeText={setQuantity}
       />
@@ -48,7 +93,9 @@ export default function addCategory({navigation }: any) {
       <View style={styles.buttonContainer}>
         <Button title="Regresar" onPress={() => navigation.goBack()} />
       </View>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
     </ScrollView>
+    
   );
 }
 
